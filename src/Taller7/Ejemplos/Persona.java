1243e4bd068ce1483404dcbc43dbc0f03b69900b
Mauro <Mauro@192.168.2.74>
2025-07-08 14:42:28 -0500

    feat: Mauro Anaya - ejemplo 3 taller 7

--- src/Taller7/Ejemplos/Persona.java
+++ src/Taller7/Ejemplos/Persona.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Taller7.Ejemplos;
 
-/**
- *
- * @author Mauro
- */
 public class Persona {
-    
+
+    public String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void mostrarNombre() {
+        System.out.println("Nombre: " + nombre);
+    }
 }
---
+++ src/Taller7/Ejemplos/Utilidades.java
@@ -0,0 +1,8 @@
+package Taller7.Ejemplos;
+
+public class Utilidades {
+
+    public static void imprimirMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
