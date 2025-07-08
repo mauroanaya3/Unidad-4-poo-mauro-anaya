5ce06ec3a12da1d4b88613de50959ba59ef19743
Mauro <Mauro@192.168.2.74>
2025-07-07 23:51:24 -0500

    feat: Mauro Anaya - ejemplo 3 taller 4

---
+++ src/Taller4/Ejemplos/Vehiculo.java
@@ -0,0 +1,26 @@
+package Taller4.Ejemplos;
+
+public class Vehiculo {
+
+    private String marca;
+    private double velocidad;
+
+    public Vehiculo(String marca, double velocidad) {
+        this.marca = marca;
+        this.velocidad = velocidad;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public void acelerar(double incremento) {
+        if (incremento > 0) {
+            velocidad += incremento;
+        }
+    }
+}
