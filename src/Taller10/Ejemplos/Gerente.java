4bce2e6fc20b0de13f63eeb433677fa37eb647d7
Mauro <Mauro@192.168.2.74>
2025-07-08 17:43:35 -0500

    feat: Mauro Anaya - ejemplo 2 taller 10

---
+++ src/Taller10/Ejemplos/Gerente.java
@@ -0,0 +1,14 @@
+
+package Taller10.Ejemplos;
+
+/**
+ *
+ * @author Mauro
+ */
+public class Gerente extends Empleado {
+
+    @Override
+    public void trabajar() {
+        System.out.println("El gerente está gestionando el equipo.");
+    }
+}
