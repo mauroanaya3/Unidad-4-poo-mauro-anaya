0267b85cb82a82eb42fae779e6cb446f532dcf2c
Mauro <Mauro@192.168.2.74>
2025-07-08 16:18:53 -0500

    feat: Mauro Anaya - ejemplo 2 taller 8

---
+++ src/Taller8/Ejemplos/Animal.java
@@ -0,0 +1,18 @@
+package Taller8.Ejemplos;
+
+public class Animal {
+
+    protected String especie;
+
+    public Animal(String especie) {
+        this.especie = especie;
+    }
+
+    public void emitirSonido() {
+        System.out.println("El animal hace un sonido.");
+    }
+
+    public void mostrarEspecie() {
+        System.out.println("Especie: " + especie);
+    }
+}
