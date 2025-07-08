d28d78b349635e6c168e36733b24c708a5d45821
Mauro <Mauro@192.168.2.74>
2025-07-08 18:39:30 -0500

    feat: Mauro Anaya - ejemplo 3 taller 11

---
+++ src/Taller11/Ejemplos/Coche.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller11.Ejemplos;
+
+/**
+ *
+ * @author Mauro
+ */
+public class Coche {
+    // Error de compilación: La clase Coche debe implementar el método abstracto conducir()
+}
---
+++ src/Taller11/Ejemplos/Vehiculo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller11.Ejemplos;
+
+/**
+ *
+ * @author Mauro
+ */
+public class Vehiculo {
+     public abstract void conducir(); // Método abstracto
+}
