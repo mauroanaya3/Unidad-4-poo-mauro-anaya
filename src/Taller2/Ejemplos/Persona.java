b371ef7ae8dde2bf3a534b42af43c24f8876123a
Mauro <Mauro@192.168.2.74>
2025-07-07 19:53:58 -0500

    feat: Mauro Anaya - ejemplo 1 taller 2

---
+++ src/Taller2/Ejemplos/Persona.java
@@ -0,0 +1,15 @@
+
+package Taller2.Ejemplos;
+
+public class Persona {
+
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre; 
+    }
+
+    public void mostrarNombre() {
+        System.out.println("Nombre: " + this.nombre); 
+    }
+}
