9806265e5ddb95b64e28867bf0b98a973040362e
Mauro <Mauro@192.168.2.74>
2025-07-08 00:06:30 -0500

    feat: Mauro Anaya - ejemplo 5 taller 4

---
+++ src/Taller4/Ejemplos/Producto.java
@@ -0,0 +1,15 @@
+package Taller4.Ejemplos;
+
+public class Producto {
+
+    private double precio;
+
+    public Producto(double precio) {
+        this.precio = precio;
+    }
+
+    public void actualizarPrecio(double nuevoPrecio) {
+
+        precio = nuevoPrecio;
+    }
+}
