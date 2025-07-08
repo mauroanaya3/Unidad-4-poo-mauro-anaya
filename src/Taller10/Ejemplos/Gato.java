8f8a58ac5d3fbe15edefd4f8607ba83db6836581
Mauro <Mauro@192.168.2.74>
2025-07-08 17:47:11 -0500

    feat: Mauro Anaya - ejemplo 5 taller 10

---
+++ src/Taller10/Ejemplos/Gato.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller10.Ejemplos;
+
+/**
+ *
+ * @author Mauro
+ */
+public class Gato {
+
+    public void hacerSonido() {
+        System.out.println("El gato maúlla.");
+    }
+}
