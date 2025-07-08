469bb8d3c2f5c44a0c48dec37d6e940abbde9e5c
Mauro <Mauro@192.168.2.74>
2025-07-08 17:42:12 -0500

    feat: Mauro Anaya - ejemplo 1 taller 10

---
+++ src/Taller10/Ejemplos/Animal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller10.Ejemplos;
+
+/**
+ *
+ * @author Mauro
+ */
+public class Animal {
+
+    public void hacerSonido() {
+        System.out.println("El animal hace un sonido.");
+    }
+}
---
+++ src/Taller10/Ejemplos/Perro.java
@@ -0,0 +1,14 @@
+
+package Taller10.Ejemplos;
+
+/**
+ *
+ * @author Mauro
+ */
+public class Perro {
+
+    @Override
+    public void hacerSonido() {
+        System.out.println("El perro ladra.");
+    }
+}
