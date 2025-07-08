ebf6de86c4e6222d0c7bbb77ab8a9680bb8cfc11
Mauro <Mauro@192.168.2.74>
2025-07-08 16:25:39 -0500

    feat: Mauro Anaya - ejemplo 6 taller 8

--- src/Taller8/Ejemplos/Animal.java
+++ src/Taller8/Ejemplos/Animal.java
@@ -2,17 +2,9 @@ package Taller8.Ejemplos;
 
 public class Animal {
 
-    protected String especie;
+    private String especie;
 
     public Animal(String especie) {
         this.especie = especie;
-    }
-
-    public void emitirSonido() {
-        System.out.println("El animal hace un sonido.");
-    }
-
-    public void mostrarEspecie() {
-        System.out.println("Especie: " + especie);
     }
 }
--- src/Taller8/Ejemplos/Perro.java
+++ src/Taller8/Ejemplos/Perro.java
@@ -2,19 +2,8 @@ package Taller8.Ejemplos;
 
 public class Perro extends Animal {
 
-    private String raza;
-
-    public Perro(String especie, String raza) {
-        super(especie); // Llama al constructor de la clase base
-        this.raza = raza;
-    }
-
-    public void ladrar() {
-        System.out.println("El perro ladra.");
-    }
-
-    @Override
-    public void emitirSonido() {
-        System.out.println("El perro ladra.");
+    public Perro(String raza) {
+        // No se llama al constructor de la clase base
+        System.out.println("Raza: " + raza);
     }
 }
