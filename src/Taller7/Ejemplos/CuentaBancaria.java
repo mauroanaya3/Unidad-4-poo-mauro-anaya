a829d186f728a0fae38b35ceef6e6116dcc1627d
Mauro <Mauro@192.168.2.74>
2025-07-08 14:49:32 -0500

    feat: Mauro Anaya - ejemplo 5 taller 7

---
+++ src/Taller7/Ejemplos/CuentaBancaria.java
@@ -0,0 +1,10 @@
+package Taller7.Ejemplos;
+
+
+public class CuentaBancaria {
+    public double saldo;
+    
+    public CuentaBancaria(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+}
