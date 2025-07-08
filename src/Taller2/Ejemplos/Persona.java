435578dfd7e28920b3eea897c4b09d06ff2cc9a0
Mauro <Mauro@192.168.2.74>
2025-07-07 20:02:13 -0500

    feat: Mauro Anaya - ejemplo 5 taller 2

--- src/Taller2/Ejemplos/Persona.java
+++ src/Taller2/Ejemplos/Persona.java
@@ -4,8 +4,13 @@ public class Persona {
 
     private String nombre;
 
-    public static void mostrarNombre() {
-        System.out.println(this.nombre);
+    public void setNombre(String nombre) {
+        this.nombre = nombre; 
+        
+        this.mostrarNombre();
+    }
 
+    public void mostrarNombre() {
+        System.out.println("Nombre: " + nombre);
     }
 }
