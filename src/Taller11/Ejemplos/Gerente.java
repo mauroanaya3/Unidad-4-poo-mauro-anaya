84931e72efbe553d2a75e208a5ca48a6bdd2e99f
Mauro <Mauro@192.168.2.74>
2025-07-08 18:35:52 -0500

    feat: Mauro Anaya - ejemplo 2 taller 11

---
+++ src/Taller11/Ejemplos/Gerente.java
@@ -0,0 +1,17 @@
+package Taller11.Ejemplos;
+
+// Clase derivada
+public class Gerente extends Empleado {
+
+    private double bono;
+
+    public Gerente(String nombre, double bono) {
+        super(nombre);
+        this.bono = bono;
+    }
+
+    @Override
+    public void calcularSalario() {
+        System.out.println("El salario del gerente incluye un bono de: " + bono);
+    }
+}
