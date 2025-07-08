6163f733bc201ebe3a67dde7d65ed3a3525d1b41
Mauro <Mauro@192.168.2.74>
2025-07-08 17:45:38 -0500

    feat: Mauro Anaya - ejemplo 4 taller 10

---
+++ src/Taller10/Ejemplos/Coche.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller10.Ejemplos;
+
+/**
+ *
+ * @author Mauro
+ */
+public class Coche {
+
+    // Error de compilación: Diferente firma del método
+    @Override
+    public void acelerar() {
+        System.out.println("El coche acelera.");
+    }
+}
---
+++ src/Taller10/Ejemplos/Vehiculo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller10.Ejemplos;
+
+/**
+ *
+ * @author Mauro
+ */
+public class Vehiculo {
+
+    public void acelerar(int velocidad) {
+        System.out.println("El vehículo acelera a " + velocidad + " km/h.");
+    }
+}
