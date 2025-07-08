eea1d96341100a6c64742626c96ab779634478c4
Mauro <Mauro@192.168.2.74>
2025-07-08 16:20:13 -0500

    feat: Mauro Anaya - ejemplo 3 taller 8

---
+++ src/Taller8/Ejemplos/Empleado.java
@@ -0,0 +1,16 @@
+package Taller8.Ejemplos;
+
+public class Empleado {
+
+    protected String nombre;
+    protected double salario;
+
+    public Empleado(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Nombre: " + nombre + ", Salario: " + salario);
+    }
+}
