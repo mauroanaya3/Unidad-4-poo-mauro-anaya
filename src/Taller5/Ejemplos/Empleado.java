ebefdea410cff1ba475d3b4fbc37d2cbe00fb632
Mauro <Mauro@192.168.2.74>
2025-07-08 10:26:01 -0500

    feat: Mauro Anaya - ejemplo 2 taller 5

---
+++ src/Taller5/Ejemplos/Empleado.java
@@ -0,0 +1,17 @@
+package Taller5.Ejemplos;
+
+// Clase con miembros de paquete (default)
+class Empleado {
+
+    String nombre;
+    double salario;
+
+    Empleado(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre + ", Salario: " + salario);
+    }
+}
