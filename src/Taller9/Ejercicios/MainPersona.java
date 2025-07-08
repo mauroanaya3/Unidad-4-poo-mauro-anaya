a07be281dbaad495166024e43d987effe0de4876
Mauro <Mauro@192.168.2.74>
2025-07-08 17:28:38 -0500

    feat: Mauro Anaya - ejercicio 1 taller 9

---
+++ src/Taller9/Ejercicios/MainPersona.java
@@ -0,0 +1,10 @@
+package Taller9.Ejercicios;
+
+// Ejercicio 1
+public class MainPersona {
+
+    public static void main(String[] args) {
+        Empleado emp = new Empleado("Juan", 30, "Ventas");
+        emp.mostrarDetalles();
+    }
+}
---
+++ src/Taller9/Ejercicios/Persona.java
@@ -0,0 +1,18 @@
+package Taller9.Ejercicios;
+
+// Ejercicio 1
+public class Persona {
+
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+}
