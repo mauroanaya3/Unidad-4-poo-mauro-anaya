20a146892f437ccbfd571127e18024685819af11
Mauro <Mauro@192.168.2.74>
2025-07-07 23:49:07 -0500

    feat: Mauro Anaya - ejemplo 1 taller 4

---
+++ src/Taller4/Ejemplos/CuentaBancaria.java
@@ -0,0 +1,24 @@
+package Taller4.Ejemplos;
+
+public class CuentaBancaria {
+
+    private double saldo; 
+
+    public CuentaBancaria(double saldoInicial) {
+        if (saldoInicial >= 0) {
+            this.saldo = saldoInicial;
+        } else {
+            this.saldo = 0;
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+        }
+    }
+}
