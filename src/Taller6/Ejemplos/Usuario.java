5f473cfcf724db2d19faa5f36f8bccf81decee72
Mauro <Mauro@192.168.2.74>
2025-07-08 12:44:05 -0500

    feat: Mauro Anaya - ejemplo 5 taller 6

---
+++ src/Taller6/Ejemplos/Usuario.java
@@ -0,0 +1,10 @@
+package Taller6.Ejemplos;
+
+public class Usuario {
+
+    protected String contraseña; // No debería ser `protected`
+
+    public Usuario(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
