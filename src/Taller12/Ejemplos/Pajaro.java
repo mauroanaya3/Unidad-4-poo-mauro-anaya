57390e61136314c01b88450addf7f190a45876d1
Mauro <Mauro@192.168.2.74>
2025-07-08 21:59:47 -0500

    feat: Mauro Anaya - ejemplo 1 taller 12

---
+++ src/Taller12/Ejemplos/Pajaro.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller12.Ejemplos;
+
+public class Pajaro implements Volador {
+
+    @Override
+    public void volar() {
+        System.out.println("El pájaro está volando.");
+    }
+}
---
+++ src/Taller12/Ejemplos/Volador.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller12.Ejemplos;
+
+public interface Volador {
+
+    void volar(); // Método abstracto
+}
