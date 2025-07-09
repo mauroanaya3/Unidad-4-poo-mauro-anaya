3502f87e66cb386bcb9ac390922f6149403526c9
Mauro <Mauro@192.168.2.74>
2025-07-08 23:01:42 -0500

    feat: Mauro Anaya - ejemplo 9 taller 12

---
+++ src/Taller12/Ejemplos/Arbol.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller12.Ejemplos;
+
+// Clase incorrecta que implementa la interfaz pero no tiene lógica relacionada
+public class Arbol implements Corredor {
+
+    @Override
+    public void correr() {
+        // El árbol no tiene una lógica para correr, lo cual no tiene sentido
+        System.out.println("Los árboles no pueden correr.");
+    }
+}
---
+++ src/Taller12/Ejemplos/Corredor.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller12.Ejemplos;
+
+// Interfaz Corredor
+public interface Corredor {
+
+    void correr();
+}
