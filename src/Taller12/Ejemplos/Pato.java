66fdbb9e83a1ac649782b28e7af0e3a34ed8f245
Mauro <Mauro@192.168.2.74>
2025-07-08 22:05:57 -0500

    feat: Mauro Anaya - ejemplo 3 taller 12

---
+++ src/Taller12/Ejemplos/Pato.java
@@ -0,0 +1,14 @@
+package Taller12.Ejemplos;
+
+public class Pato implements Volador, Nadador {
+
+    @Override
+    public void volar() {
+        System.out.println("El pato está volando.");
+    }
+
+    @Override
+    public void nadar() {
+        System.out.println("El pato está nadando.");
+    }
+}
